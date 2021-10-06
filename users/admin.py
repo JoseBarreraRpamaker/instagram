dfd52590dd116d09db2f101437f4abfc3b401d63
Your Name <jose@rpamaker.com>
2021-10-06 17:03:41 -0300

    list_filter

--- users/admin.py
+++ users/admin.py
@@ -12,3 +12,10 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display= ('pk','user','phone_number','website','pcture')
     list_display_links=('pk','user')
     list_editable=('website','pcture','phone_number')
+    search_fields=('user__email','user__username')
+    list_filter=('created','user__is_active')
+    fieldsets=(
+          ('profile'),{
+          'fields' : ('user','picture'),
+           
+          }),
