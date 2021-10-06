1ea251794a42c46c7421cc32c0fe68576895c2a8
Your Name <jose@rpamaker.com>
2021-10-06 10:52:22 -0300

    agregaciond de tablas en base de datos

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)   # blank puede estar vacio el campo.
+
+    birthdat = models.DateField(blank=True,null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
