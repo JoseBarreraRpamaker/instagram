509ea48e49a038043c9916c8fa4981f757e2fea6
Your Name <jose@rpamaker.com>
2021-10-06 16:38:47 -0300

    actualizo

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Profile
+
+from django.contrib import admin
+
+# Register your models herec
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display= ('pk','user','phone_number','website','pcture')
+    list_display_links=('pk','user')
+    list_editable=('website','pcture','phone_number')
