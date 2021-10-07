23ed0934d7f46b681efa5c16a197d8f12ab498e7
Your Name <jose@rpamaker.com>
2021-10-07 13:33:41 -0300

    tengo un error

--- posts/models.py
+++ posts/models.py
@@ -1,21 +1,21 @@
-from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.base import Model
 from django.contrib.auth.models import User
 
-
-from users.models import Profile
-# Create your models here.
 
 class Post(models.Model):
+ 
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='posts/photos')
 
-    created = models.DateField(auto_now_add=True)
-    modified = models.DateField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+      
+        return '{} by @{}'.format(self.title, self.user.username)
 
 
--- users/models.py
+++ users/models.py
@@ -1,50 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import User, update_last_login
-from django.db.models.fields import NullBooleanField
 
+class Profile(models.Model):
+    
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Users(models.Model):
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
 
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    is_admin = models.BooleanField(default=False)
-
-    bio = models.TextField(blank=True)   # blank puede estar vacio el campo.
-
-    birthdat = models.DateField(blank=True,null=True)
+    picture = models.ImageField(
+        upload_to='users/pictures',
+        blank=True,
+        null=True
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
-
-
-
-
-class Profile(models.Model):
-
-
-    user = models.OneToOneField(User , on_delete=models.CASCADE)
-    website = models.URLField(max_length=200,blank=True) 
-    biography = models.TextField(blank=True)
-    phone_number = models.CharField(max_length=15,blank=True)
-
-
-    pcture = models.ImageField(upload_to='users/pictures',blank=True,null=True)
-
-    created = models.DateTimeField(auto_now=True)
-    modified = models.DateTimeField(auto_now=True)
-
-
-
-def __str__(self):
-    return self.user.username
+    def __str__(self):
+       
+        return self.user.username
 
 
  
